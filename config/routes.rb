3c4b77d475fd6f7b64cd00d173d25ce75be4fbf5
Duc Tai TRAN <tdts2010@gmail.com>
2023-12-06 17:34:23 +0100

    page 404 et 500 faits, ne marchent qu'en prod a verifier sur heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   resources :compares
   resources :searches, only: [:new, :create, :show, :destroy] do
     resources :results, only: [:new, :create, :show]
@@ -9,4 +11,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # get "up" => "rails/health#show", as: :rails_health_check
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
